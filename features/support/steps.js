f1617860684367a9914719e9995184cd66ea2ed6
luminescent <christina.androne@gmail.com>
2020-03-27 15:53:08 +0000

    semi working example

---
+++ features/support/steps.js
@@ -0,0 +1,33 @@
+// features/support/steps.js
+const { Given, When, Then, BeforeAll, AfterAll, setDefaultTimeout } = require("cucumber");
+const { expect } = require("chai");
+const { openBrowser, $, goto, focus, click, write, into, contains, checkBox, textBox, text, toRightOf, toLeftOf, closeBrowser, waitFor, below } = require('taiko');
+
+
+setDefaultTimeout(30 * 1000); // 30s
+
+BeforeAll(function() {
+    return openBrowser({headless: false});
+})
+
+AfterAll(function() {
+    return closeBrowser();
+})
+
+Given("a variable set to {int}", function(number) {
+  this.setTo(number);
+  return goto("https://google.com");
+});
+
+When("I increment the variable by {int}", function(number) {
+  this.incrementBy(number);
+  return write(`${this.variable} + ${number} =`)
+    .then(click("Google Search"));
+});
+
+Then("the variable should contain {int}", async function(number) {
+  expect(this.variable).to.eql(number);
+
+  const result = await text(`${number}`).exists();
+  expect(result).to.eql(true);
+});
